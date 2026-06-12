9c83ce63afb38c478256fced58fff2aa3ed4859b
sandilinhtet <sandilinhtetmdy@gmail.com>
2026-06-12 17:15:07 +0900

    add

---
+++ src/main/java/com/example/EmployeeAttendanceApp/EmployeeAttendanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeAttendanceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAttendanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAttendanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EmployeeAttendanceApp/EmployeeAttendanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeAttendanceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAttendanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
